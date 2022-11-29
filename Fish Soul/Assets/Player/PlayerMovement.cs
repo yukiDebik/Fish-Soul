45e1a43dace417fbbcef3d3da9c183c8b9798322
Абрамов Александр Алесеевич <abramov.aa0789@okeit.edu>
2022-11-29 14:13:59 +0500

    Анимации игрока.
    Созданы анимации игрока, игрок движется в 4-х направлениях: вверх, вниз, вправо и влево.

--- Fish Soul/Assets/Player/PlayerMovement.cs
+++ Fish Soul/Assets/Player/PlayerMovement.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    public float speed = 4f;
-    private Vector2 direction;
-    private Rigidbody2D rb;
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
 
     void Start()
     {
@@ -16,47 +17,19 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        direction.x = Input.GetAxis("Horizontal");
-        direction.y = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-    
-    /*
-    private Rigidbody2D rb;
-    public float baseSpeed = 4f;
-    public float sprint = 9f;
-    private float currentSpeed;
-    private Vector2 direction;
-    //private Vector2 movement = new Vector2(0f, 0f);
- 
-    void Start ()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
- 
-    void Update ()
-    {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            currentSpeed = sprint;
-        }
-        else
-        {
-            currentSpeed = baseSpeed;
-        }
-        direction.x = Input.GetAxis("Horizontal");
-        direction.y = Input.GetAxis("Vertical");
 
-    }
- 
-    void FixedUpdate ()
-    {
-        //rb.MovePosition(rb.position + direction * currentSpeed * Time.fixedDeltaTime);
-        //rb.MovePosition(rb.position + direction * baseSpeed * currentSpeed * Time.fixedDeltaTime);
-    }
-    */
 }
