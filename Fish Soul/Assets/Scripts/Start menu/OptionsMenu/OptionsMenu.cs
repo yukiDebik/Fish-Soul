6d0d9c54e5ff4d5936b3323c63354cf0502d76a5
yukiDebik <yotokomaru@gmail.com>
2022-12-02 13:24:12 +0500

    Мини-игра
    Создание мини-игры "Рыбалка".

---
+++ Fish Soul/Assets/Scripts/Start menu/OptionsMenu/OptionsMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    
+    public void SetVolume (float volume)
+    {
+        Debug.Log("Volume!");
+    }
+
+}
