b0cd8a87ed29b98948cd346e272511e622e3736f
yukiDebik <yotokomaru@gmail.com>
2022-12-14 13:19:11 +0500

    Запуск сцены по триггеру.
    Запускаем сцены, когда игрок входит в определённую зону.

---
+++ Fish Soul/Assets/Scripts/LevelOne/TriggerFishing.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TriggerFishing : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("You have entered the fishing area.");
+        SceneManager.LoadScene("FishingMinigame", LoadSceneMode.Additive);
+        //object.GetComponent<PlayerMovement>().enabled = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("You embroidered from the fishing zone.");
+    }
+
+}
