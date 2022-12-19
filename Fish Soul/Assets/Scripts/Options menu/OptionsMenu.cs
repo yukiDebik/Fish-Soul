6e7e00f320ce36db10e19281e0076c949d62ca30
yukiDebik <yotokomaru@gmail.com>
2022-12-19 12:41:05 +0500

    Options menu.

--- Fish Soul/Assets/Scripts/Options menu/OptionsMenu.cs
+++ Fish Soul/Assets/Scripts/Options menu/OptionsMenu.cs
@@ -1,12 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 using UnityEngine;
 
 public class OptionsMenu : MonoBehaviour
 {
-    
-    public void SetVolume (float volume)
+
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
     {
-        Debug.Log(volume);
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRate + "Hz";
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.RefreshShownValue();
+        LoadSettings(currentResolutionIndex);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    
+    public void ExitSettings()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
+        PlayerPrefs.SetInt("FullscreenPreference", System.Convert.ToInt32(Screen.fullScreen));
+    }
+
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+            resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
+        else
+            resolutionDropdown.value = currentResolutionIndex;
+
+        if (PlayerPrefs.HasKey("FullscreenPreference"))
+            Screen.fullScreen = System.Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
+        else
+            Screen.fullScreen = true;
     }
 }
